146732c52c3fcc2e7e50ab6261d72e63d50a7f01
priyankarumane08 <pmrumane8622@gmail.com>
2026-09-23 18:16:52 +0530

    feat: add authentication and Axios API setup

---
+++ api/auth.js
@@ -0,0 +1,11 @@
+import api from "@/lib/axios";
+
+export async function loginUser(username, password) {
+  const response = await api.post("/auth/login", {
+    username,
+    password,
+    expiresInMins: 30
+  });
+
+  return response.data;
+}
